68835ba2cc92a7dadb09a9ec94b3c31d80b21c24
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-10 17:21:38 -0600

    Add append, count and to_string methods

--- lib/node.rb
+++ lib/node.rb
@@ -6,4 +6,31 @@ class Node
     @data  = data
     @next_node = nil
   end
+
+  def append(node)
+    if @next_node.nil?
+      @next_node = node
+    else
+      @next_node.append
+    end
+  end
+
+  def count(total)
+    if @next_node.nil?
+      total
+    else
+      total += 1
+      @next_node.count(total)
+    end
+  end
+
+  def to_string(string)
+    string = "#{string} " + @data
+
+    if @next_node.nil?
+      string.strip
+    else
+      @next_node.to_string(string)
+    end
+  end
 end
