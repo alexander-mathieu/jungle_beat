e2eac19fede1ebad8d8bde933954e4b3f9d525bc
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-10 18:35:03 -0600

    Add specs for find, includes? and pop methods

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -66,5 +66,38 @@ RSpec.describe LinkedList do
 
       expect(@linked_list.to_string).to eq('Node 1 Node 3 Node 2')
     end
+
+    it '.find' do
+      @linked_list.append('Node 1')
+      @linked_list.append('Node 2')
+      @linked_list.append('Node 3')
+      @linked_list.append('Node 4')
+      @linked_list.append('Node 5')
+
+      expect(@linked_list.find(2, 1)).to eq('Node 3')
+      expect(@linked_list.find(1, 3)).to eq('Node 2, Node 3, Node 4')
+    end
+
+    it '.includes?' do
+      @linked_list.append('Node 1')
+      @linked_list.append('Node 2')
+
+      expect(@linked_list.includes?('Node 1')).to eq(true)
+      expect(@linked_list.includes?('Node 2')).to eq(true)
+      expect(@linked_list.includes?('Node 4')).to eq(false)
+    end
+
+    it '.pop' do
+      @linked_list.append('Node 1')
+      @linked_list.append('Node 2')
+      @linked_list.append('Node 3')
+      @linked_list.append('Node 4')
+      @linked_list.append('Node 5')
+
+      expect(@linked_list.pop).to eq('Node 5')
+      expect(@linked_list.pop).to eq('Node 4')
+
+      expect(@linked_list.to_string).to eq('Node 1 Node 2 Node 3')
+    end
   end
 end
