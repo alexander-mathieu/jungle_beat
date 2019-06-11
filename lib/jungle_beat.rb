4d17e8cfce14ec165413090f9ac60b665c7cf175
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-11 13:58:47 -0600

    Add rate, voice, reset rate and reset voice methods, refactor append and prepend methods

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,13 +1,41 @@
 class JungleBeat
-  attr_reader :list
+  attr_reader   :list, :rate, :voice
+  attr_accessor :rate, :voice
 
   def initialize
-    @list = new_linked_list
+    @list  = new_linked_list
+    @rate  = 500
+    @voice = "Alex"
+  end
+
+  def all
+    @list.to_string
   end
 
   def append(stringified_nodes)
     node_data_array = stringified_nodes.split(' ')
-    node_data_array.each { |node_data| @list.append(node_data) }
+    count = 0
+
+    node_data_array.each do |node_data|
+      if valid_beats.include?(node_data)
+        count += 1
+        @list.append(node_data)
+      end
+    end
+    count
+  end
+
+  def prepend(stringified_nodes)
+    node_data_array = stringified_nodes.split(' ')
+    count = 0
+
+    node_data_array.each do |node_data|
+      if valid_beats.include?(node_data)
+        count += 1
+        @list.prepend(node_data)
+      end
+    end
+    count
   end
 
   def count
@@ -15,14 +43,27 @@ class JungleBeat
   end
 
   def play
-    beats = @list.to_string
+    `say -r #{@rate} -v #{@voice} #{all}`
+  end
 
-    `say -r 200 #{beats}`
+  def reset_rate
+    @rate = 500
+  end
+
+  def reset_voice
+    @voice = "Alex"
   end
 
   private
 
   def new_linked_list
     LinkedList.new
+  end
+
+  def valid_beats
+    ['tee', 'dee', 'deep',
+     'bop', 'boop', 'la',
+     'deep', 'doo', 'ditt',
+     'woo', 'hoo', 'shu']
   end
 end
