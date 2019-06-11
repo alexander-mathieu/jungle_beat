b480e5d9e253c0fc70aa7638ce264c44fa6a1dd3
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-06-10 18:07:08 -0600

    Refactor Node class

--- lib/node.rb
+++ lib/node.rb
@@ -1,15 +1,15 @@
 class Node
-  attr_reader :data,
-              :next_node
+  attr_reader   :data
+  attr_accessor :next_node
 
   def initialize(data)
     @data  = data
     @next_node = nil
   end
 
-  def append(node)
+  def append(data)
     if @next_node.nil?
-      @next_node = node
+      @next_node = Node.new(data)
     else
       @next_node.append
     end
